bb55e6e7c1e412cc17c8589b2c6df32cb560d4f0
Kryword <Kryword@gmail.com>
2017-12-21 00:22:26 +0100

    Implementada parte del init y accept de la clase SignalTask. Falta por implementar el cálculo de los valores A y B.

--- src/upnasignal/SignalTask.java
+++ src/upnasignal/SignalTask.java
@@ -5,9 +5,17 @@
  */
 package upnasignal;
 
+import app.ContactsComponentInterface;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.net.InetAddress;
+import java.net.Socket;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
 
 /**
  *
@@ -15,20 +23,90 @@ import java.net.InetAddress;
  */
 public class SignalTask {
 
+    static final String INTERACTION = "signal";
+    final InetAddress inetAddress;
+    final int port;
+    DataInputStream in;
+    DataOutputStream out;
+    final String nickname;
+    
     SignalTask(InetAddress inetAddress, int port, String nickname) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.inetAddress = inetAddress;
+        this.port = port;
+        this.nickname = nickname;
+        this.in = null;
+        this.out = null;
     }
 
     SignalTask(DataInputStream in, DataOutputStream out, String nickname) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        this.in = in;
+        this.out = out;
+        this.nickname = nickname;
+        this.inetAddress = null;
+        this.port = -1;
     }
 
     protected void init(){
-        
+        try {
+            Socket socket = new Socket(inetAddress, port);
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            
+            Messages messages = new Messages();
+            // Primera parte del protocolo
+            out.writeUTF(messages.getHeadingMessage(nickname, INTERACTION));
+            
+            String response = in.readUTF();
+            Boolean result = messages.parseResultStatusMessage(response);
+            Boolean bye = messages.parseBYEMessage(in.readUTF());
+            if (!result && bye){
+                System.out.println("Cliente cierra conexión.");
+                socket.close();
+                return;
+            }
+            
+            // Segunda parte del protocolo Calculo de A
+            
+            
+        } catch (IOException ex) {
+            Logger.getLogger(SignalTask.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(SignalTask.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
     protected void accept() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try{
+            // Primera parte del protocolo. Verifico si el usuario está en la base de datos y si no devuelvo falso
+            Messages messages = new Messages();
+            ContactsComponentInterface contactsInterface = new ContactsComponentInterface();
+            Boolean result = null;
+            try{
+                Future<Boolean> future = contactsInterface.isInContacts(nickname);
+                Boolean isInContacts = future.get();
+                result = isInContacts;
+            } catch (InterruptedException ex) {
+                Logger.getLogger(SignalTask.class.getName()).log(Level.SEVERE, null, ex);
+                result = false;
+            } catch (ExecutionException ex) {
+                Logger.getLogger(SignalTask.class.getName()).log(Level.SEVERE, null, ex);
+                result = false;
+            }finally{
+                out.writeUTF(messages.getResultStatusMessage(result));
+                if (result){
+                    out.writeUTF(messages.getACKMessage());
+                }else{
+                    out.writeUTF(messages.getBYEMessage());
+                    in.close();
+                    out.close();
+                    return;
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(SignalTask.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(SignalTask.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
