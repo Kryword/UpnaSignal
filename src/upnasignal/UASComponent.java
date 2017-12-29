7c527ddd5069d1fd874c859e2c5dd5f99dad76e5
Kryword <Kryword@gmail.com>
2017-12-29 23:05:00 +0100

    Completado el componente UASComponent, aún faltan algunas inconsistencias por completar. Dejaré estas inconsistencias para cuando reciba la respuesta a mi correo. Hago esto debido a que la entrega es mañana y no creo que pueda estar a la mañana.

--- src/upnasignal/UASComponent.java
+++ src/upnasignal/UASComponent.java
@@ -9,6 +9,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import static java.util.concurrent.TimeUnit.MICROSECONDS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -35,13 +36,13 @@ public class UASComponent implements Runnable{
     @Override
     public void run(){
         try(final ServerSocket serverSocket = new ServerSocket(port)){
-            final ExecutorService executor = Executors.newFixedThreadPool(numThreads);
-            
+            final ExecutorService executorForSockets = Executors.newFixedThreadPool(numThreads);
+            final ExecutorService executorForInputMessages = Executors.newSingleThreadExecutor();
             final Callable<Socket> waitConnectionTask = () -> serverSocket.accept();
             
             while (isRunning){
                 Socket socket = null;
-                final Future<Socket> future = executor.submit(waitConnectionTask);
+                final Future<Socket> future = executorForSockets.submit(waitConnectionTask);
                 do{
                     try{
                         socket = future.get(100, MICROSECONDS);
@@ -56,9 +57,23 @@ public class UASComponent implements Runnable{
                 
                 if (future.isDone()){
                     LOGGER.info("Mensaje entrante");
+                    final HeadingMessageProcessor hMessage = new HeadingMessageProcessor(socket);
+                    executorForInputMessages.submit(hMessage);
                 }
             }
-            executor.shutdown();
+            executorForSockets.shutdown();
+            executorForInputMessages.shutdown();
+                int j = 0;
+                do {
+                    boolean x;
+                    try {
+                        x = executorForInputMessages.awaitTermination(2500, MILLISECONDS);
+                    } catch (final InterruptedException ex) {
+                        break;
+                    }
+                    LOGGER.log(Level.INFO, "** Intento {0} {1}", new Object[]{j, x});
+                    ++j;
+                } while (!executorForInputMessages.isTerminated());
         } catch (IOException ex) {
             Logger.getLogger(UASComponent.class.getName()).log(Level.SEVERE, null, ex);
         }
